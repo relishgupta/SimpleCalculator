87751556a71e4f1823dcb55d3cae9dcba7b4f1e7
relishgupta <guptarelish333@gmail.com>
2023-01-26 02:10:49 +0530

    yes

---
+++ script.js
@@ -0,0 +1,24 @@
+let screenText="";
+const btn=document.querySelectorAll(".button");
+Array.from(btn).forEach((button)=>{
+    button.addEventListener('click',(e)=>{
+        exp=e.target.innerHTML;
+        if(exp=='='){
+            screenText=eval(screenText);
+            document.querySelector("input").value=screenText;
+
+        }
+        else if(exp=='C'){
+            screenText="";
+            document.querySelector("input").value=screenText;
+        }
+        else if(exp=='B'){
+            screenText=screenText.substring(0,screenText.length-1);
+            document.querySelector("input").value=screenText;
+        } 
+        else{
+            screenText=screenText+exp;
+            document.querySelector("input").value=screenText;
+        }
+    })
+})
